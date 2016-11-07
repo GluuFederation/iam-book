e9e6a4d8dd564af300ab167db5379b69f1756282
Meghna <ldeveloperl1985@gmail.com>
2016-11-07 20:12:49 +0530

    Setting file changes

--- oAuthExample/app.js
+++ oAuthExample/app.js
@@ -9,16 +9,17 @@ var settings = require("./settings");
 var client_id = settings.client_id;
 var client_secret = settings.client_secret;
 var port = settings.port;
-var redirectURI = settings.redirectURI;
+var redirectURI = settings.baseURI + settings.redirectURIPath;
+var redirectURIPath =  settings.redirectURIPath;
 var urlShortenScope = "https://www.googleapis.com/auth/urlshortener";
 
 var oauth2Client = new OAuth2(
     client_id,
     client_secret,
-    redirectURI
+    redirectURI/* This should match the Redirect URL you have set in the google api console project*/
 );
 
-app.use(bodyParser.urlencoded({There
+app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.set('views', path.join(__dirname, 'views'));
@@ -26,23 +27,21 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.json());
 
+// This method will be called when you visit site  
 app.get('/', function(req, res) {
-    res.render('index');
+    res.render('index'); // This will render views/index.ejs page to the user
 })
 
-app.get('/processredirect', function(req, res) {
+// This method will call when google redirect after generating the code
+app.get(redirectURIPath, function(req, res) {
     res.render('demourlshorten', {
-        access_token: req.query.code
-    });
+        access_code: req.query.code
+    }); // This will render views/demourlshorten.ejs to the user along with code which required to get the access token to call the google API
 });
 
-app.get('/urlshorten', function(req, res) {
-    console.log('Req ' + req.getHeader("access_token"));
-    res.json("recieved Data" + req.getHeader("access_token"));
-})
-
+// This method will be called from views/index.ejs as form submit action to intiate the authentication process with google.
 app.post('/requestoAuth', function(req, res) {
-    var scopes = [urlShortenScope];5
+    var scopes = [urlShortenScope];
     var url = oauth2Client.generateAuthUrl({
         access_type: 'offline',
         scope: scopes
@@ -51,6 +50,7 @@ app.post('/requestoAuth', function(req, res) {
     res.redirect(url);
 });
 
+// This method is called from the views/demourlshorten.ejs as ajax request for the user to create the shortenURL 
 app.post('/shortenURL', function(req, res) {
     console.log("Creating client");
     var oauth2Client1 = new OAuth2(
@@ -59,10 +59,14 @@ app.post('/shortenURL', function(req, res) {
         redirectURI
     );
 
+    // This will request google OAuth2 server for the tokens for calling URLShorten API
     oauth2Client.getToken(req.body.token, function(err, tokens) {
-        console.log("Obtaining token");
+        
         if (!err) {
+            console.log("Token Obtained setting it to called the URL shorten API");
             oauth2Client.setCredentials(tokens);
+            
+            // Creating client for URL shortenURL API Call
             var urlshortener = google.urlshortener({
                 version: 'v1',
                 auth: oauth2Client
@@ -73,16 +77,19 @@ app.post('/shortenURL', function(req, res) {
                 }
             }, function(err, data) {
                 if (err) {
+                    console.log("Error occur while obtaining the shorten url");
                     res.status(500).json({
                         "err": err
                     });
                 } else {
-                    res.json({
+                    console.log("Shorten URL recieved");
+                    res.status(200).json({
                         "data": data
                     });
                 }
             });
         } else {
+            console.log("Error occur while obtaining the token");
             res.status(500).json({
                 "err": err
             });
--- oAuthExample/settings.js
+++ oAuthExample/settings.js
@@ -1,4 +1,5 @@
-exports.client_id = /* Client ID from Google Console Project */
-exports.client_secret = /* Client ID from Google Console Project */
-exports.port = /* PORT  */
-exports.redirectURI = /* REDIRECT URI  */;
+exports.client_id =  /* Client ID from Google Console Project */;
+exports.client_secret =  /* Client Secret from Google Console Project */;
+exports.port = /**/;
+exports.redirectURIPath = /**/;
+exports.baseURI = /*Host*/
